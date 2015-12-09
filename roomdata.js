202f39fa9cb0225c58f8164012ae4c85331622ce
michaeldegroot <specamps@gmail.com>
2015-12-09 11:17:38 +0100

    full coverage

--- roomdata.js
+++ roomdata.js
@@ -1,4 +1,4 @@
-var Debug = false;
+exports.Debug = false;
 
 exports.rooms = {};
 
@@ -8,12 +8,12 @@ exports.roomExists = function(socket, room) {
 };
 
 exports.createRoom = function(socket, room) {
-	if(Debug) console.log(socket.id+": Creating Room: "+room);
+	if(exports.Debug) console.log(socket.id+": Creating Room: "+room);
 	this.rooms[room] = {owner:socket.id, users:[], variables: {}};
 }
 
 exports.set = function(socket, variable, content) {
-	if(Debug) console.log(socket.id+": Creating variable: "+variable+" with content: "+content);
+	if(exports.Debug) console.log(socket.id+": Creating variable: "+variable+" with content: "+content);
 	if(!this.roomExists(socket, socket.roomdata_room)){
 		console.error("You have tried setting a room variable but this socket is not in any room!");
 		return false;
@@ -22,7 +22,7 @@ exports.set = function(socket, variable, content) {
 }
 
 exports.get = function(socket, variable, content) {
-	if(Debug) console.log(socket.id+": Getting variable: "+variable);
+	if(exports.Debug) console.log(socket.id+": Getting variable: "+variable);
 	if(variable == "room"){
 		if(!socket.roomdata_room) return undefined;
 		return socket.roomdata_room;
@@ -37,7 +37,7 @@ exports.get = function(socket, variable, content) {
 }
 
 exports.joinRoom = function(socket, room) {
-	if(Debug) console.log(socket.id+": Joining room: "+room);
+	if(exports.Debug) console.log(socket.id+": Joining room: "+room);
 	if(socket.roomdata_room) this.leaveRoom(socket, room);
 	if(!this.roomExists(socket, room)) this.createRoom(socket, room);
 	this.rooms[room].users.push(socket.id);
@@ -51,7 +51,8 @@ exports.clearRoom = function(room) {
 
 exports.leaveRoom = function(socket) {
 	var room = socket.roomdata_room;
-	if(Debug) console.log(socket.id+": Leaving room: "+socket.roomdata_room);
+	if(socket.roomdata_room==undefined) throw new Error("socket id:" + socket.id + " is not in a room!");
+	if(exports.Debug) console.log(socket.id+": Leaving room: "+socket.roomdata_room);
 	var i = this.rooms[socket.roomdata_room].users.indexOf(socket.id);
 	if(i != -1) this.rooms[socket.roomdata_room].users.splice(i, 1);
 	socket.leave(socket.roomdata_room);
--- test/test.js
+++ test/test.js
@@ -6,6 +6,8 @@ var roomdata = require('../roomdata');
 var socket;
 var clientSocket;
 
+roomdata.Debug = true;
+
 describe("Socket.io", function(){
 	it('Connect', function(done){
 		socket = clientIo.connect('http://localhost:8080');
@@ -15,11 +17,34 @@ describe("Socket.io", function(){
 		});
 	});
 });
-describe("Roomdata internal", function(){
+describe("Internal", function(){
+	it('Set a variable while not in a room', function(){
+		assert.equal(roomdata.set(clientSocket, "gamedata","TEST"),false);
+	});
+	it('Get a variable while not in a room', function(){
+		assert.equal(roomdata.get(clientSocket, "gamedata","TEST"),undefined);
+	});
+	it('Get room of room while not in a room', function(){
+		assert.equal(roomdata.get(clientSocket, "room"),undefined);
+	});
+	it('Leave a room while not in one', function(){
+		assert.throws(function(){
+			roomdata.leaveRoom(clientSocket);
+		},Error);
+	});
 	it('Join a room', function(){
 		assert.doesNotThrow(function(){
 			roomdata.joinRoom(clientSocket, "testroom");
 		},Error);
+	});
+	it('Get room of room', function(){
+		assert.equal(roomdata.get(clientSocket, "room"),"testroom");
+	});
+	it('Get owner of room', function(){
+		assert.equal(roomdata.get(clientSocket, "owner").length,20);
+	});
+	it('Get users of room', function(){
+		assert.object(roomdata.get(clientSocket, "users"));
 	});
 	it('Set a variable in the room', function(){
 		assert.doesNotThrow(function(){
@@ -28,6 +53,11 @@ describe("Roomdata internal", function(){
 	});
 	it('Does the created variable exists?', function(){
 		assert.equal(roomdata.get(clientSocket, "gamedata"),"TEST");
+	});
+	it('Join a room while in a room already', function(){
+		assert.doesNotThrow(function(){
+			roomdata.joinRoom(clientSocket, "testroom2");
+		},Error);
 	});
 	it('Leave a room', function(){
 		assert.doesNotThrow(function(){
